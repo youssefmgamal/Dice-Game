f50f0da968d9a2b926e9ef07080948f4408facd2
Youssefmgamal <youssef_mgamal@hotmail.com>
2024-11-21 18:53:57 +0200

    edit

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-var randomNumber1 = Math.floor(Math.random() * 6) + 1;
-
-var randomNumber2 = Math.floor(Math.random() * 6) + 1;
 
 // solution one over simplified find solution two below
 // if (randomNumber1 === 1) {
@@ -42,24 +39,29 @@ var randomNumber2 = Math.floor(Math.random() * 6) + 1;
 // }
 // solution 2 easier way
 
-var randomImageSource1 = "images/dice" + randomNumber1 + ".png";
-var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
+document.querySelector(".btn").addEventListener("click", refreshPage);
+
+function refreshPage() {
+    
+    var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+
+    var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+
+    var randomImageSource1 = "images/dice" + randomNumber1 + ".png";
+    var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
 
 document.querySelectorAll("img")[0].setAttribute("src", randomImageSource1);
 document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
 
-
-if (randomNumber1 > randomNumber2) {
-    document.querySelector("h1").innerHTML = "🚩 Player 1 Wins!";
+    if (randomNumber1 > randomNumber2) {
+        document.querySelector("h1").innerHTML = "🚩 Player 1 Wins!";
+    }
+    else if (randomNumber1 < randomNumber2) {
+        document.querySelector("h1").innerHTML = "Player 2 Wins! 🚩";
+    }
+    else {
+        document.querySelector("h1").innerHTML = "Draw!";
+    }
 }
-else if (randomNumber1 < randomNumber2) {
-    document.querySelector("h1").innerHTML = "Player 2 Wins! 🚩";
-}
-else {
-    document.querySelector("h1").innerHTML = "Draw!";
-}
-
-document.querySelector(".btn").addEventListener("click", function() {
-    location.reload();
-});
 
